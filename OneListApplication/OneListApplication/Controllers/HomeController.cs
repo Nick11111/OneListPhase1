a5a716b9c4d64abcc44a87b9e4c3f05c9c208653
A01000789 <A01000789@DTC01L0382-21>
2016-12-12 14:09:09 -0800

    edit the index page, create a css file for all style sheets, create a image folder to store all images.

--- OneListApplication/OneListApplication/Controllers/HomeController.cs
+++ OneListApplication/OneListApplication/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace OneListApplication.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
