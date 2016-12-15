51b8677689c9dcc81f309e53e2e208d1ee9f785c
Max <A00558617@DTC01L0382-22>
2016-12-15 10:37:53 -0800

    Add subscriber pages

---
+++ OneListApplication/OneListApplication/Controllers/SubscriberController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneListApplication.Controllers
+{
+    public class SubscriberController : Controller
+    {
+        // GET: Subscriber
+        public ActionResult AddSubscriber()
+        {
+            return View();
+        }
+
+        public ActionResult SubGroupDetails()
+        {
+            return View();
+        }
+
+        public ActionResult EditSubGroup()
+        {
+            return View();
+        }
+
+    }
+}
