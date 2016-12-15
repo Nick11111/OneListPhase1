85a1ce6526560d926ee87b245b3ab88363e72f18
A01000789(Nick) <A01000789@DTC01L0382-21>
2016-12-15 12:36:20 -0800

    Add item pages and finish itemManagement page and CreateItem page

---
+++ OneListApplication/OneListApplication/Controllers/ItemController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneListApplication.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult ItemManagement()
+        {
+            return View();
+        }
+        public ActionResult CreateItem()
+        {
+            return View();
+        }
+        public ActionResult CreateItemGroup()
+        {
+            return View();
+        }
+        public ActionResult ItemGroupManagement()
+        {
+            return View();
+        }
+        
+    }
+}
