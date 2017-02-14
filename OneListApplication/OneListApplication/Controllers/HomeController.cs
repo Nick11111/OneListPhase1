005e5c20151ff24e0e55a8685e312c2b5c40aead
Nick <rainliu1991@gmail.com>
2017-02-14 13:03:33 -0800

    plit the login and register page into two distinct pages, add all the packages for web security, implement the login and register modules with sql databse(no assigning roles and forgotten passord yet)

--- OneListApplication/OneListApplication/Controllers/HomeController.cs
+++ OneListApplication/OneListApplication/Controllers/HomeController.cs
@@ -5,24 +5,243 @@ using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using OneListApplication.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
 
 namespace OneListApplication.Controllers
 {
     public class HomeController : Controller
     {
+        const string EMAIL_CONFIRMATION = "EmailConfirmation";
+        const string PASSWORD_RESET = "ResetPassword";
+
+        void CreateTokenProvider(UserManager<IdentityUser> manager, string tokenType)
+        {
+            manager.UserTokenProvider = new EmailTokenProvider<IdentityUser>();
+        }
         // GET: Home
         public ActionResult Index()
         {
             ViewBag.date = GetBuildDate();
             return View();
         }
-
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Login(LoginVM login)
+        {
+            // UserStore and UserManager manages data retreival.
+            UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> manager
+            = new UserManager<IdentityUser>(userStore);
+            IdentityUser identityUser = manager.Find(login.UserName, login.Password);
 
-        public ActionResult Home()
+            if (ModelState.IsValid)
+            {
+                if (ValidLogin(login))
+                {
+                    IAuthenticationManager authenticationManager
+                                           = HttpContext.GetOwinContext()
+                                            .Authentication;
+                    authenticationManager
+                   .SignOut(DefaultAuthenticationTypes.ExternalCookie);
+
+                    var identity = new ClaimsIdentity(new[] {
+                                            new Claim(ClaimTypes.Name, login.UserName),
+                                        },
+                                        DefaultAuthenticationTypes.ApplicationCookie,
+                                        ClaimTypes.Name, ClaimTypes.Role);
+                    // SignIn() accepts ClaimsIdentity and issues logged in cookie. 
+                    authenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = false
+                    }, identity);
+                    return RedirectToAction("Home", "Home");
+                }
+            }
+            return View();
+        }
+
+        bool ValidLogin(LoginVM login)
+        {
+            UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(userStore)
+            {
+                UserLockoutEnabledByDefault = true,
+                DefaultAccountLockoutTimeSpan = new TimeSpan(0, 10, 0),
+                MaxFailedAccessAttemptsBeforeLockout = 3
+            };
+            var user = userManager.FindByName(login.UserName);
+
+            if (user == null)
+                return false;
+
+            // User is locked out.
+            if (userManager.SupportsUserLockout && userManager.IsLockedOut(user.Id))
+                return false;
+
+            // Validated user was locked out but now can be reset.
+            if (userManager.CheckPassword(user, login.Password) && userManager.IsEmailConfirmed(user.Id))
+            {
+                if (userManager.SupportsUserLockout
+                 && userManager.GetAccessFailedCount(user.Id) > 0)
+                {
+                    userManager.ResetAccessFailedCount(user.Id);
+                    System.Threading.Thread.Sleep(2000);
+                    return true;
+                }
+                System.Threading.Thread.Sleep(2000);
+                return true;
+            }
+            // Login is invalid so increment failed attempts.
+            else
+            {
+                bool lockoutEnabled = userManager.GetLockoutEnabled(user.Id);
+                if (userManager.SupportsUserLockout && userManager.GetLockoutEnabled(user.Id))
+                {
+                    userManager.AccessFailed(user.Id);
+                    return false;
+                }
+                return false;
+            }
+
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisteredUserVM newUser)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore)
+            {
+                UserLockoutEnabledByDefault = true,
+                DefaultAccountLockoutTimeSpan = new TimeSpan(0, 10, 0),
+                MaxFailedAccessAttemptsBeforeLockout = 3
+            };
+            var identityUser = new IdentityUser()
+            {
+                UserName = newUser.UserName,
+                Email = newUser.Email
+            };
+            IdentityResult result = manager.Create(identityUser, newUser.Password);
+
+            if (result.Succeeded)
+            {
+                CreateTokenProvider(manager, EMAIL_CONFIRMATION);
+
+                var code = manager.GenerateEmailConfirmationToken(identityUser.Id);
+                var callbackUrl = Url.Action("ConfirmEmail", "Home",
+                                                new { userId = identityUser.Id, code = code },
+                                                    protocol: Request.Url.Scheme);
+
+                string email = "Please confirm your account by clicking this link: <a href=\""
+                                + callbackUrl + "\">Confirm Registration</a>";
+                //SendGrid.sendEmail(newUser, callbackUrl);
+                ViewBag.FakeConfirmation = email;
+            }
+            return View();
+        }
+        public ActionResult ConfirmEmail(string userID, string code)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
+            var user = manager.FindById(userID);
+            CreateTokenProvider(manager, EMAIL_CONFIRMATION);
+            try
+            {
+                IdentityResult result = manager.ConfirmEmail(userID, code);
+                if (result.Succeeded)
+                    ViewBag.Message = "You are now registered!";
+            }
+            catch
+            {
+                ViewBag.Message = "Validation attempt failed!";
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult SecureArea()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            var authenticationManager = ctx.Authentication;
+            authenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ForgotPassword(string email)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
+            var user = manager.FindByEmail(email);
+            CreateTokenProvider(manager, PASSWORD_RESET);
+
+            var code = manager.GeneratePasswordResetToken(user.Id);
+            var callbackUrl = Url.Action("ResetPassword", "Home",
+                                         new { userId = user.Id, code = code },
+                                         protocol: Request.Url.Scheme);
+            ViewBag.FakeEmailMessage = "Please reset your password by clicking <a href=\""
+                                     + callbackUrl + "\">here</a>";
+            //SendGrid.sendResetEmail(user.Email, user.UserName, callbackUrl);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ResetPassword(string userID, string code)
+        {
+            ViewBag.PasswordToken = code;
+            ViewBag.UserID = userID;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ResetPassword(string password, string passwordConfirm,
+                                          string passwordToken, string userID)
+        {
+
+            var userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
+            var user = manager.FindById(userID);
+            CreateTokenProvider(manager, PASSWORD_RESET);
+
+            if (password == passwordConfirm)
+            {
+                IdentityResult result = manager.ResetPassword(userID, passwordToken, password);
+                if (result.Succeeded)
+                {
+                    ViewBag.Result = "The password has been reset.";
+                }
+                else
+                {
+                    ViewBag.Result = "The password has not been reset.";
+                }
+            }
+
+            else
+                ViewBag.Result = "Two passwords don't match!";
+            return View();
+        }
+
+    public ActionResult Home()
         {
             return View();
         }
---
+++ OneListApplication/OneListApplication/Models/LoginVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OneListApplication.Models
+{
+  
+        public class LoginVM
+        {   
+            [Required(ErrorMessage = "Username is required")]
+            //[Required(ErrorMessage = "Email is required")]
+            //[RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$",
+            //ErrorMessage = "This is not a valid email address.")]
+            public string UserName { get; set; }
+
+            [Required(ErrorMessage = "Password is required")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+}
